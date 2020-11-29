e0f7a267cde5eeb3bdea1ca22c0ef3b3266db40b
TolkachevPeter <peter.tolkachev@gmail.com>
2020-11-29 14:25:09 +0300

    add new import

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import Header from './Header'
 import Main from './Main'
+import Footer from './Footer'
 
 function App() {
   return (
   <div className="root">
       <Header />
       <Main />
+      <Footer />
   </div>
   );
 }
