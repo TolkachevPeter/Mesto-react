fb6f38e3373f1335869bd0c059894283d7a35a32
TolkachevPeter <peter.tolkachev@gmail.com>
2020-11-29 21:39:17 +0300

    fix Main

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,32 +1,10 @@
 import React from 'react';
 
-function PopupWithForm() {
-    // function handleEditAvatarClick() {
-    //     document.querySelector('#popup-edit').classList.toggle('popup_is-opened');
-    // }
-    function handleEditProfileClick() {
-        document.querySelector('#popup-edit').classList.toggle('popup_is-opened');
-    }
-    function handleAddPlaceClick() {
-        document.querySelector('#popup-new').classList.toggle('popup_is-opened');
-    }
+function PopupWithForm(props) {
 
   return (
       <div>
-    <div className="profile root__section">
-      <div className="user-info">
-        <div className="user-info__photo"></div>
-        <div className="user-info__data">
-          <h1 className="user-info__name">Jaques Causteau</h1>
-          <p className="user-info__job">Sailor, Researcher</p>
-          <button className="button user-info__button-edit" onClick={handleEditProfileClick}>Edit</button>
-        </div>
-        <button className="button user-info__button" onClick={handleAddPlaceClick}>+</button>
-      </div>
-    </div>
-    <div className="places-list root__section">
 
-    </div>
     <div className="popup" id='popup-new'>
       <div className="popup__content">
         <img src="<%=require(./images/close.svg)%>" alt="" className="popup__close" />
