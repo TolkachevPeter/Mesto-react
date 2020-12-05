bec5bf811e9325b18829b38d802f19e41a09775f
TolkachevPeter <peter.tolkachev@gmail.com>
2020-12-06 00:42:23 +0300

    add api data

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PopupWithForm from './PopupWithForm';
+import api from '../utils/api';
 
 function Main(props) {
 
@@ -18,8 +19,8 @@ function Main(props) {
       <div className="user-info">
         <div className="user-info__photo"></div>
         <div className="user-info__data">
-          <h1 className="user-info__name">Jaques Causteau</h1>
-          <p className="user-info__job">Sailor, Researcher</p>
+  <h1 className="user-info__name">{userName}</h1>
+  <p className="user-info__job">{userDesciption}</p>
           <button className="button user-info__button-edit" onClick={onEditProfile}>Edit</button>
         </div>
         <button className="button user-info__button" onClick={onAddPlace}>+</button>
